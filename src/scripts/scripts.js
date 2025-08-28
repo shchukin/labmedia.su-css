732f7852b784b3350f571c02abe4eaa609cea38a
Anton Shchukin <a.a.shchukin@gmail.com>
2025-08-28 11:58:40 +0700

    init template

---
+++ src/scripts/scripts.js
@@ -0,0 +1,59 @@
+(function($) {
+
+    /* Inputs */
+
+    /* Select placeholder */
+    function selectPlaceholder($element) {
+        if ($element.val() === 'placeholder') {
+            $element.parent('.input').addClass('input--placeholder-is-chosen');
+        } else {
+            $element.parent('.input').removeClass('input--placeholder-is-chosen');
+        }
+    }
+
+    $('select.input__widget').each(function () {
+        selectPlaceholder($(this));
+    }).on('change', function () {
+        selectPlaceholder($(this));
+    });
+
+    /* Expanding textarea */
+    function expandTextarea($element) {
+        $element.css('height', 'auto');
+        $element.css('height', ($element[0].scrollHeight + 2 * parseInt($element.css('border-width'), 10)) + 'px');
+    }
+
+    $('.input--expandable .input__widget').each(function () {
+        expandTextarea($(this));
+    }).on('input', function () {
+        expandTextarea($(this));
+    });
+
+    /* Error field */
+    $('.input__widget').on('focus', function () {
+        $(this).parents('.input').removeClass('input--error');
+        $(this).parents('.input').nextUntil(':not(.helper--error)').remove();
+    });
+
+
+
+    /* Init magnific popup */
+
+    $('.mfp-handler').magnificPopup({
+        type: 'inline',
+        removalDelay: 200,
+        showCloseBtn: false,
+        callbacks: {
+            open: function() {
+                const $popup = $.magnificPopup.instance.content;
+                const $expandableInputs = $popup.find('.input--expandable .input__widget');
+                $expandableInputs.each(function() {
+                    expandTextarea($(this));
+                });
+            }
+        }
+    });
+
+
+
+})(jQuery);
