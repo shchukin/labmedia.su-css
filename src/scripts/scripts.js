5a7714e89d3ddc36b7f2e8972cb5c32402246e07
Anton Shchukin <a.a.shchukin@gmail.com>
2025-09-02 13:51:10 +0700

    header scripts

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,6 +1,7 @@
 (function($) {
 
     const $html = $('html');
+    let rememberedPageScrollPosition = 0;
 
     /* Inputs */
 
@@ -158,13 +159,23 @@
     $(document).ready(scrolling);
 
     $('.header__burger').on('click', function () {
-        $html.toggleClass('burger-expanded');
+
+        if( ! $html.hasClass('burger-expanded') ) {
+            rememberedPageScrollPosition = $(window).scrollTop(); /* Запомнить скролл пользователя, так как display: none на .page его сбросит (смотри .burger-expanded .page) */
+            $html.addClass('burger-expanded');
+            $(window).scrollTop(0); /* При открытии меню его скролл должен быть в начале */
+        } else {
+            $html.removeClass('burger-expanded');
+            $('.nav__item').removeClass('nav__item--expanded');
+            $(window).scrollTop(rememberedPageScrollPosition);/* При закрытии меню скролл должен быть там, где пользователь его оставил */
+        }
     });
 
 
     $(document).on('click', function(event) {
         if (!$(event.target).closest('.header').length) {
             $html.removeClass('burger-expanded');
+            $(window).scrollTop(rememberedPageScrollPosition);/* При закрытии меню скролл должен быть там, где пользователь его оставил */
         }
     });
 
