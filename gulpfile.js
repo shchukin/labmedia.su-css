ab1664afcb3ea9eff8e513583200aab7f00c33dd
Anton Shchukin <a.a.shchukin@gmail.com>
2025-09-03 10:58:05 +0700

    build

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,6 @@ var postcss = require('gulp-postcss');
 var postcssPresetEnv = require('postcss-preset-env');
 var postcssHoverMediaFeature = require('postcss-hover-media-feature');
 var base64 = require('gulp-base64');
-var svgstore = require('gulp-svgstore');
-var svgmin = require('gulp-svgmin');
 var change = require('gulp-change');
 
 
@@ -26,80 +24,6 @@ function addSourcesTimestamp(content) {
             return line;
         }
     }).join('\n');
-}
-
-
-function symbolsImgToSpriteSvg(content) {
-
-    var source = content.split('\n');
-    var outputLine = [];
-    var result = '';
-
-    var i;
-    var indentString;
-    var classString;
-    var idString;
-    var widthString;
-    var heightString;
-    var titleString;
-    var srcString;
-    var pathString;
-    var nameString;
-    var timestamp = Math.round(new Date().getTime() / 1000);
-
-    source.forEach(function (line) {
-
-        if (line.indexOf('symbols/') !== -1) {
-
-            /* get indent */
-
-            for (indentString = '', i = 0; i < line.indexOf('<img'); i++) {
-                indentString += ' ';
-            }
-
-
-            /* get attributes */
-
-            classString  = line.match('class[ \t]*=[ \t]*"[^"]+"');
-            idString     = line.match(    'id[ \t]*=[ \t]*"[^"]+"');
-            widthString  = line.match( 'width[ \t]*=[ \t]*"[^"]+"');
-            heightString = line.match('height[ \t]*=[ \t]*"[^"]+"');
-            titleString  = line.match( 'title[ \t]*=[ \t]*"[^"]+"');
-
-            classString  = classString  ? classString[0]  : null;
-            idString     = idString     ? idString[0]     : null;
-            widthString  = widthString  ? widthString[0]  : null;
-            heightString = heightString ? heightString[0] : null;
-            titleString  = titleString  ? titleString[0]  : null;
-
-
-            /* get path and name */
-
-            srcString = line.match('src[ \t]*=[ \t]*"[^"]+"');
-            srcString = srcString[0];
-            srcString = srcString.replace('src="', '');
-            srcString = srcString.replace('"', '');
-
-            nameString = srcString.replace(/^.*[\\\/]/, '');
-            nameString = nameString.replace('.svg', '');
-
-            pathString = srcString.replace(nameString + '.svg', '');
-
-
-            /* write down results */
-
-            outputLine[0] = indentString + '<svg' + (classString ? ' ' + classString : '') + (idString ? ' ' + idString : '') + (widthString ? ' ' + widthString : '') + (heightString ? ' ' + heightString : '') + '>';
-            outputLine[1] = indentString + '    ' + '<use xlink:href="' + pathString + 'symbols.svg?' + timestamp + '#' + nameString + '"></use>';
-            outputLine[2] = indentString + '</svg>';
-
-            result += outputLine[0] + '\n' + outputLine[1] + '\n' + outputLine[2] + '\n';
-        } else {
-            result += line + '\n';
-        }
-
-    });
-
-    return result;
 }
 
 
@@ -165,24 +89,22 @@ gulp.task('images', function () {
 });
 
 
-// Markups: copy and change symbols <img> to sprite <svg>
+// Markups: copy, add timestamps
 
 gulp.task('markups', function () {
     return gulp.src('src/markups/**/*', {encoding: false})
         .pipe(plumber())
-        .pipe(change(symbolsImgToSpriteSvg))
         .pipe(change(addSourcesTimestamp))
         .pipe(gulp.dest('build/markups/'))
         ;
 });
 
 
-// Layouts: copy and change symbols <img> to sprite <svg>
+// Layouts: copy, add timestamps
 
 gulp.task('layouts', function () {
     return gulp.src('src/layouts/**/*', {encoding: false})
         .pipe(plumber())
-        .pipe(change(symbolsImgToSpriteSvg))
         .pipe(change(addSourcesTimestamp))
         .pipe(gulp.dest('build/layouts/'))
         ;
@@ -213,17 +135,6 @@ gulp.task('scripts', function () {
 });
 
 
-// Symbols
-
-gulp.task('symbols', function () {
-    return gulp.src('src/symbols/*.svg', {encoding: false})
-        .pipe(plumber())
-        .pipe(svgmin())
-        .pipe(svgstore())
-        .pipe(gulp.dest('build/symbols/'));
-});
-
-
 // Styles: concat, add prefixes, compress, copy
 
 gulp.task('styles', function () {
@@ -241,8 +152,8 @@ gulp.task('styles', function () {
         }))
         .pipe(postcss(processors))
         .pipe(base64({
-            maxImageSize: 200 * 1024, // 200KB threshold
-            exclude: ['/sprite/', '/images/'] // Allow files from /vectors/ only
+            maxImageSize: 20 * 1024, // 200KB threshold
+            exclude: ['images']
         }))
         .pipe(gulp.dest('build/styles/'))
         .pipe(size())
@@ -269,7 +180,7 @@ gulp.task('lint', function () {
 
 
 gulp.task('default', function (fn) {
-    run('clean', 'manifest', 'favicon', 'temp', 'content', 'images', 'markups', 'layouts', 'vendors', 'scripts', 'symbols', 'styles', 'lint', fn);
+    run('clean', 'manifest', 'favicon', 'temp', 'content', 'images', 'markups', 'layouts', 'vendors', 'scripts', 'styles', 'lint', fn);
 });
 
 
